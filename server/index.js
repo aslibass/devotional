9db17d8af826651092fa2a9677ea21a7e77353f6
Viren Joseph <viren.joseph@gmail.com>
2026-02-14 18:07:14 +1100

    Fix Express 5.x wildcard route compatibility

--- server/index.js
+++ server/index.js
@@ -123,7 +123,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, '../dist')));
 
 // Handle client-side routing - send all non-API requests to index.html
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
